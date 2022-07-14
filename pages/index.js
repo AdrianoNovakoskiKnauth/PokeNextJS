47fd4f451758688472bff825954936b0d05d9c1f
adriano_nokoski@hotmail.com <adriano_novakoski@hotmail.com>
2022-07-14 18:24:44 -0300

    Criado as requisições da lista de pokemons e suas imagens, Card de exibição dos pokemons

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
+import Image from 'next/image'
+import Styles from '../styles/Home.module.css'
+import Card from './components/card'
 
-import styles from '../styles/Home.module.css'
+export async function getStaticProps() {
+  const maxPokemon = 200
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
 
-export default function Home() {
+  const res = await fetch(`${api}/?limit=${maxPokemon}`)
+  const data = await res.json()
+
+  //add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    }
+  }
+
+}
+
+export default function Home({pokemons}) {
   return (
-    <h1>POKENEXT</h1>
+    <>
+      <div className={Styles.title_container}>
+        <h1 className={Styles.title}>Poke<span>Next</span></h1>
+        <Image src='/images/pokeball.png' width='50' height='50' alt='Pokeball' />
+      </div>
+      <div className={Styles.pokemon_container}>
+        {pokemons.map((pokemon) => (
+          <Card key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+    </>
   )
 }
